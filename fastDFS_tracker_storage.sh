1a4209743e447707fe1ae4081d7ab3a14b699c9a
lzw <kxbbjsw@126.com>
2017-02-21 10:13:53 +0800

    change fastdfs-nginx-module

--- fastDFS_tracker_storage.sh
+++ fastDFS_tracker_storage.sh
@@ -22,9 +22,6 @@ cd /software/fastDFS/fastdfs-5.05
 ./make.sh
 ./make.sh install
 
-cp /etc/fdfs/tracker.conf.sample /etc/fdfs/tracker.conf
-cp /etc/fdfs/storage.conf.sample /etc/fdfs/storage.conf
-
 cd /software/fastDFS/
 tar zxvf fastdfs-nginx-module_v1.16.tar.gz
 
@@ -33,6 +30,17 @@ mv config config_bak
 wget https://raw.githubusercontent.com/lizhiwei666/lzw_fastDFS/master/softwarefastDFSfastdfs-nginx-modulesrc_config
 mv softwarefastDFSfastdfs-nginx-modulesrc_config config
 cd /software/fastDFS/
+
+cp /etc/fdfs/tracker.conf.sample /etc/fdfs/tracker.conf
+cp /etc/fdfs/storage.conf.sample /etc/fdfs/storage.conf
+cp /etc/fdfs/client.conf.sample /etc/fdfs/client.conf
+cp /software/fastDFS/fastdfs-5.05/conf/mime.types /etc/fdfs
+cp /software/fastDFS/fastdfs-5.05/conf/http.conf /etc/fdfs
+cp /software/fastDFS/fastdfs-nginx-module/src/mod_fastdfs.conf /etc/fdfs
+mkdir -p /home/yuqing/fastdfs/fastdfs-nginx-module/data-and-log
+
+
+
 
 
 
